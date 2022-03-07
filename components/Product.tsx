31e34509843e8fdd2b59a965b579fa3ec622574e
sebastian-jurek <sebastian.jurek44@gmail.com>
2022-03-07 15:51:35 +0100

    add products

--- components/Product.tsx
+++ components/Product.tsx
@@ -18,7 +18,16 @@ interface ProductProps {
 export const ProductDetails = ({ data }: ProductProps) => {
   return (
     <>
-      <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
+      <div className="bg-white p-4">
+        <Image
+          src={data.thumbnailUrl}
+          alt={data.thumbnailAlt}
+          layout={"responsive"}
+          height={1}
+          width={4}
+          objectFit="contain"
+        />
+      </div>
       <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
       <p className="p-4">{data.description}</p>
       <Rating rating={data.rating} />
